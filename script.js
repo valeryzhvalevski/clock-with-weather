145b1f49923b37a2add0ecc6e16c6d731a8758c3
valeryzhvalevski <105601886+valeryzhvalevski@users.noreply.github.com>
2024-03-19 11:22:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// Задание 1
+// Вывести в консоль ТОЛЬКО 5 сообщений с интервалом 2 секунды:
+// Сообщение номер 1
+// Сообщение номер 2
+// Сообщение номер 3
+// Сообщение номер 4
+// Сообщение номер 5
+
+// const messages = [
+//     'Сообщение номер 1',
+//     'Сообщение номер 2',
+//     'Сообщение номер 3',
+//     'Сообщение номер 4',
+//     'Сообщение номер 5'
+//   ];
+  
+//   messages.forEach((message, index) => {
+//     setTimeout(() => {
+//       console.log(message);
+//     }, (index + 1) * 2000);
+//   });
+  
+
+//   Задание 2
+// Сделать виджет - цифровые часы, оформить по желанию.
+// Вам нужно будет каждую секунду запускать функцию, которая будет создавать новый объект Date и забирать из него необходимую информацию.
+
+function updateClock() {
+    const now = new Date();
+    const hours = ('0' + now.getHours()).slice(-2);
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+    const seconds = ('0' + now.getSeconds()).slice(-2);
+    const timeFull = hours + '<span class="blink">:</span>' + minutes + '<span class="blink">:</span>' + seconds;
+    document.getElementById('clock').innerHTML = timeFull;
+  }
+  
+  setInterval(updateClock, 1000);
+  updateClock();
+  
+  const clock = document.getElementById('clock')
+  const weather = document.getElementById('weather')
+  const actualWeather = document.getElementById('actual-weather')
+
+  weather.style.display = 'none'
+  clock.addEventListener('mouseover', () => {
+    weather.style.display = 'block'
+  })
+  clock.addEventListener('mouseout', () => {
+    weather.style.display = 'none'
+  })
+
+
+  const city = "Minsk";
+  const apiKey = "31e317c69034405b046f44f15c1886a1";
+  
+  function getWeatherByCity(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const temperature = Math.round(data.main.temp);
+        const weatherDescription = data.weather[0].description;
+        actualWeather.textContent = `${temperature}°C, ${weatherDescription}`;
+      })
+      .catch(error => {
+        console.error("Ошибка при получении данных о погоде:", error);
+      });
+  }
+  
+  getWeatherByCity(city);
